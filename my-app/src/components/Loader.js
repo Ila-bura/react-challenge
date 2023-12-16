52d5fa81ce465d0c96d28e5152017947736f6e67
Ila-bura <>
2023-12-16 13:58:54 +0000

    Add loader wheel

---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import css from "./css/Loader.module.css";
+
+// Functional component
+function Loader() {
+  return (
+    <div className= {css.Loader}>
+
+    </div>
+  );
+};
+
+export default Loader;
